4ac11e1fda1069efdec60330b8dad7bfa5cef8b0
Fabian Thorsen <thorsen.fabian@gmail.com>
2021-02-17 22:38:10 +0100

    Minor change to signin.styles.css

--- src/components/CustomButton/CustomButton.component.jsx
+++ src/components/CustomButton/CustomButton.component.jsx
@@ -4,7 +4,7 @@ import './CustomButton.styles.scss';
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
   return (
     <button
-      className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+      className={`${isGoogleSignIn ? 'google-sign-in ' : ''}custom-button`}
       {...otherProps}
     >
       {children}
